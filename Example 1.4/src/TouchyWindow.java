3bcb770cd873eb64a5b42c4d1eec7c471e84e7a4
Seth Battis <SethBattis@stmarksschool.org>
2013-09-17 11:58:04 -0400

    Example 1.4

---
+++ Example 1.4/src/TouchyWindow.java
@@ -0,0 +1,14 @@
+import objectdraw.*;
+import java.awt.*;
+
+public class TouchyWindow extends WindowController {
+
+	public void onMousePress(Location point) {
+		Text myText = new Text("I'm Touched", 40, 50, canvas);
+		
+	}
+	
+	public void onMouseRelease(Location point) {
+		canvas.clear();
+	}
+}
