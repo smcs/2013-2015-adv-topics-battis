aade42d121b1c236b2945949a145ff353b132912
Seth Battis <SethBattis@stmarksschool.org>
2013-09-17 21:39:45 -0400

    minor edit

--- Example 1.4/src/TouchyWindow.java
+++ Example 1.4/src/TouchyWindow.java
@@ -5,7 +5,6 @@ public class TouchyWindow extends WindowController {
 
 	public void onMousePress(Location point) {
 		Text myText = new Text("I'm Touched", 40, 50, canvas);
-		
 	}
 	
 	public void onMouseRelease(Location point) {
